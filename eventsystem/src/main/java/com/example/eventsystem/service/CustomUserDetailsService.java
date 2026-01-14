c22a4f5995140b17a09e5eba561e37a1d0dbb07c
MenuriMadhushika <menurimadhushika@gmail.com>
2026-01-14 16:47:10 +0530

    Add service file 1

---
+++ eventsystem/src/main/java/com/example/eventsystem/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.eventsystem.service;
+
+import com.example.eventsystem.entity.User;
+import com.example.eventsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        // We convert our "User" entity to a Spring Security "UserDetails" object
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
